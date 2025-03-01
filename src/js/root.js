40c3cfb8f4d5b88f6fa8152a723688454f29679d
xue0211 <zhengtai1123.qq.com>
2025-03-02 01:42:34 +0800

    加载图片完成

--- src/js/root.js
+++ src/js/root.js
@@ -5,20 +5,18 @@ const app = new Vue({
             info: {
                 name: '余韵之',
                 job: '前端工程师',
-                live: '南京工业大学',
+                school: '南京工业大学',
                 telephone: '18964022629',
                 email: 'zx2723708118@163.com',
                 wechat: 'lambert1129',
                 age: '25',
             },
-            statement: {
-                content: ''
-            },
+          
             projects: [
                 {
                     name: 'flurryUI(基于Vue3)',
                     useSkill: 'Vue3.js、Vue Router、Vue CLI、Vite、TypeScript、SASS、MVVM、Prism.js、响应式布局 、Git管理',
-                    imgLink: '../img/flurry.jpg',
+                    imgLink: './img/flurry.jpg',
                     sourceLink: 'https://github.com/xue0211/flurryUI',
                     previewLink: 'https://xue0211.github.io/flurry-ui/',
                     describe: '本项目使用Vite搭建官网，通过Vue3、Vue Router、TypeScript等技术栈完成了Switch、Button、Dialog、Tabs组件的设计与实现，并在官网中撰写了文档。'
@@ -26,7 +24,7 @@ const app = new Vue({
                 {
                     name:'sleetUI(基于Vue2)',
                     useSkill:'Vue2.js、Vue Router、JavaScript、SCSS、Event Bus、MVVM、响应式布局、Parcel、Chai、VuePress',
-                    imgLink:'../img/sleet.jpg',
+                    imgLink:'./img/sleetUI.jpg',
                     sourceLink:'https://github.com/xue0211/gulu-demo',
                     previewLink:'',
                     describe:'本项目使用VuePress搭建官网，通过Vue2、Vue Router、JavaScript等技术栈完成了Button、Collapse、Grid、Input、Layout、Popover、Tabs、Toast组件的设计与实现，借助Event Bus进行组件通信，通过Chai进行断言测试。'
@@ -34,7 +32,7 @@ const app = new Vue({
                 {
                     name:'CNode社区(基于Vue2)',
                     useSkill:'Vue2.js、Vue Router、Vue CLI、Axios、jQuery、localStorage、filter、JavaScript、CSS、MVVM、响应式布局、Parcel',
-                    imgLink:'../img/CNode.jpg',
+                    imgLink:'./img/CNode.jpg',
                     sourceLink:'https://github.com/xue0211/CNode',
                     previewLink:'https://xue0211.github.io/CNode/cnode-project/dist/index.html#/',
                     describe:'本项目使用Vue CLI搭建官网，v-bind 动态绑定样式，Vue Router进行前端路由的切换及传参。引入Axios获取数据，运用filter对数据实现过滤处理，computed属性完成数据筛选，localStorage 技术存储数据。使用jQuery实现Pagination组件功能， 。'
@@ -42,7 +40,7 @@ const app = new Vue({
                 {
                     name:'画一只皮卡丘',
                     useSkill:'JavaScript、jQuery、Prism.js、Rem响应式页面 、CSS',
-                    imgLink:'../img/Pikachu.jpg',
+                    imgLink:'./img/Pikachu.jpg',
                     sourceLink:'https://github.com/xue0211/Pikachu-2.0',
                     previewLink:'https://xue0211.github.io/Pikachu-2.0/index.html',
                     describe:'动态向<style>标签和<pre>标签添加文本，绘制出皮卡丘的过程。用css3实现动画的效果，prism.js实现代码高亮的效果，rem实现响应式布局。'
@@ -50,7 +48,7 @@ const app = new Vue({
                 {
                     name:'Canvas画板',
                     useSkill:'原生JavaScript、Canvas、移动端、SVG、特性检测',
-                    imgLink:'../img/Canvas.jpg',
+                    imgLink:'./img/Canvas.jpg',
                     sourceLink:'https://github.com/xue0211/canvas',
                     previewLink:'https://xue0211.github.io/canvas/index.html',
                     describe:'本项目使用原生JS实现，主要调用 Canvas API，实现了线粗、调色、橡皮擦、一键删除、保存等功能。其中，context.clearRect()实现了橡皮檫和清屏的功能，className切换实现了笔的线粗、颜色切换的功能，用meta:vp、特性检测、ontouch事件实现了触屏设备与web端兼容。'
