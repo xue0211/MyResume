83e988d982f3cb929fcc934142549f5204185a69
xue0211 <zhengtai1123.qq.com>
2025-03-04 13:02:27 +0800

    更改目录

--- root.js
+++ root.js
@@ -16,7 +16,7 @@ const app = new Vue({
                 {
                     name: '《flurryUI》(基于Vue3)',
                     useSkill: 'Vue3.js、Vue Router、Vue CLI、Vite、TypeScript、SASS、MVVM、Prism.js、响应式布局 、Git管理',
-                    imgLink: './img/flurryUI.jpg',
+                    imgLink: '/img/flurryUI.jpg',
                     sourceLink: 'https://github.com/xue0211/flurryUI',
                     previewLink: 'https://xue0211.github.io/flurry-ui/',
                     describe: '本项目使用Vite搭建官网，通过Vue3、Vue Router、TypeScript等技术栈完成了Switch、Button、Dialog、Tabs组件的设计与实现，并在官网中撰写了文档。'
@@ -32,7 +32,7 @@ const app = new Vue({
                 {
                     name:'《CNode社区》(基于Vue2)',
                     useSkill:'Vue2.js、Vue Router、Vue CLI、Axios、jQuery、localStorage、filter、JavaScript、CSS、MVVM、响应式布局、Parcel',
-                    imgLink:'./img/CNode.jpg',
+                    imgLink:'/img/CNode.jpg',
                     sourceLink:'https://github.com/xue0211/CNode',
                     previewLink:'https://xue0211.github.io/CNode/cnode-project/dist/index.html#/',
                     describe:'本项目使用Vue CLI搭建官网，v-bind 动态绑定样式，Vue Router进行前端路由的切换及传参。引入Axios获取数据，运用filter对数据实现过滤处理，computed属性完成数据筛选，localStorage 技术存储数据。使用jQuery实现Pagination组件功能， 。'
@@ -40,7 +40,7 @@ const app = new Vue({
                 {
                     name:'《画一只皮卡丘》',
                     useSkill:'JavaScript、jQuery、Prism.js、Rem响应式页面 、CSS',
-                    imgLink:'./img/Pikachu.jpg',
+                    imgLink:'/img/Pikachu.jpg',
                     sourceLink:'https://github.com/xue0211/Pikachu-2.0',
                     previewLink:'https://xue0211.github.io/Pikachu-2.0/index.html',
                     describe:'动态向<style>标签和<pre>标签添加文本，绘制出皮卡丘的过程。用css3实现动画的效果，prism.js实现代码高亮的效果，rem实现响应式布局。'
@@ -48,7 +48,7 @@ const app = new Vue({
                 {
                     name:'《Canvas画板》',
                     useSkill:'原生JavaScript、Canvas、移动端、SVG、特性检测',
-                    imgLink:'./img/Canvas.jpg',
+                    imgLink:'/img/Canvas.jpg',
                     sourceLink:'https://github.com/xue0211/canvas',
                     previewLink:'https://xue0211.github.io/canvas/index.html',
                     describe:'本项目使用原生JS实现，主要调用 Canvas API，实现了线粗、调色、橡皮擦、一键删除、保存等功能。其中，context.clearRect()实现了橡皮檫和清屏的功能，className切换实现了笔的线粗、颜色切换的功能，用meta:vp、特性检测、ontouch事件实现了触屏设备与web端兼容。'
@@ -86,6 +86,10 @@ const app = new Vue({
                     describe:'能够使用Vue.js完成需求，了解Vue.js的数据双向绑定、父子兄弟组件间的通信原理，能够使用Vue-Router制作前端路由，能够使用keep-alive进行缓存。'
                 }, 
                 {
+                    name:'Git版本管理工具',
+                    describe:'了解git，能够使用git的命令行进行代码版本的管理，并写了相应博客。'
+                }, 
+                {
                     name:'MVC、Observer设计模式',
                     describe:'了解 MVC、Observer 设计模式，能够运用到实际的开发中。'
                 }, 
@@ -108,14 +112,10 @@ const app = new Vue({
                 {
                     name:'HTTP',
                     describe:'了解 HTTP 基础知识，了解常见状态码含义，能够根据请求查看响应。'
-                }, 
-                {
-                    name:'JSONP',
-                    describe:'了解JSONP的概念，知道它的使用逻辑。'
                 },    
                 {
                     name:'AJAX、Promise',
-                    describe:'了解AJAX、promise 的概念，并写了相应的博客。'
+                    describe:'了解AJAX、promise 的概念，并能够自己写出带有promise的AJAX请求。'
                 }, 
                 {
                     name:'Cookie和Session',
@@ -129,9 +129,9 @@ const app = new Vue({
                     name:'Web性能优化',
                     describe:'了解常见的web性能优化方案，并写了相应的博客。'
                 },   
-                 {
+                {
                     name:'同源策略、跨域',
-                    describe:'了解同源策略，以及常用的跨域方法。'
+                    describe:'了解同源策略，以及常用的跨域方法，比如JSONP、CORS、postMessage。'
                 }, 
             
             ],
